fae2cf11041f060d35497dfaa804289a9f142c7f
Kenneth Elde Skjellvik <kennes17@uia.no>
2021-02-08 02:42:18 +0100

    Added seekbar for work and pause. Also added loops.
    Added seekbar for work and pause. Also added loops.

--- Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -4,92 +4,116 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var timer:CountDownTimer
+    lateinit var pauseTimer:CountDownTimer
     lateinit var startButton:Button
-    lateinit var stopButton:Button
-    lateinit var coutdownDisplay:TextView
-
-    lateinit var btn30min:Button
-    lateinit var btn60min:Button
-    lateinit var btn90min:Button
-    lateinit var btn120min:Button
-
-    //Default value in ms for countdown
-    var timeToCountDownInMs = 5000L
-    val timeTicks = 1000L
+    lateinit var countdownDisplay:TextView
+    lateinit var countdownDisplayPause:TextView
+    lateinit var loopText:EditText
 
 
-    var isCounting = false
+    var workCountDownTimeInMs = 5000L
+    var pauseCountDownTimeInMs = 0L
+    val timeTicks:Long = 1000L
+    val oneMinute:Long = 600L
+    var numberOfLoops:Int = 0
+    var isCounting:Boolean = false
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        coutdownDisplay = findViewById<TextView>(R.id.countDownView)
 
-        btn30min = findViewById(R.id.btn30)
-        btn30min.setOnClickListener() {
-            updateCount(60000L*30L)
-        }
+        loopText = findViewById<EditText>(R.id.editLoops)
 
-        btn60min = findViewById(R.id.btn60)
-        btn60min.setOnClickListener() {
-            updateCount(60000L*60L)
-        }
+        // Work countdown
+        var workTimeInMinutes = 0
+        val workSeekBar = findViewById<SeekBar>(R.id.seekBarWork)
+        workSeekBar?.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
 
-        btn90min = findViewById(R.id.btn90)
-        btn90min.setOnClickListener() {
-            updateCount(60000L*90L)
-        }
+            override fun onProgressChanged(
+                    seek: SeekBar,
+                    progress: Int, fromUser: Boolean
+            ) {
+                workTimeInMinutes = progress
 
-        btn120min = findViewById(R.id.btn120)
-        btn120min.setOnClickListener() {
-            updateCount(60000L*120L)
-        }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                //Not in use in this code
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Toast.makeText(this@MainActivity,
+                        "Work for " + workTimeInMinutes + " minutes",
+                        Toast.LENGTH_SHORT).show()
+                setWorkTimer(oneMinute * workTimeInMinutes.toLong())
+            }
+        })
+
+        // Pause countdown
+        var pauseTimeInMinutes = 0
+        val pauseSeekBar = findViewById<SeekBar>(R.id.seekBarBreak)
+        pauseSeekBar?.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                    seek: SeekBar,
+                    progress: Int, fromUser: Boolean
+            ) {
+                pauseTimeInMinutes = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+                //Not in use in this code
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Toast.makeText(this@MainActivity,
+                        "A " + pauseTimeInMinutes + " minute break",
+                        Toast.LENGTH_SHORT).show()
+                setPauseTimer(oneMinute * pauseTimeInMinutes.toLong())
+            }
+        })
+
 
         startButton = findViewById<Button>(R.id.startCountdownButton)
-        startButton.setOnClickListener(){
-            if (!isCounting) {
+        startButton.setOnClickListener {
+            numberOfLoops = loopText.text.toString().toInt()
+            // If a countdown is running, you can't start another one
+            if(!isCounting){
                 startCountDown(it)
             }
-            startButton.isEnabled = false
-            isCounting = true
-        }
 
-        // Stops the timer and resets it. So user doesn't have to wait 30, 60, 90 or 120 min for it to finish.
-        stopButton = findViewById(R.id.stopButton)
-        stopButton.setOnClickListener() {
-            if (isCounting) {
-                isCounting = false
-                timer.cancel()
-                updateCount(0)
-                startButton.isEnabled = true
-            }
         }
+        countdownDisplay = findViewById<TextView>(R.id.countDownView)
+        countdownDisplayPause = findViewById<TextView>(R.id.countDownViewPause)
     }
 
-    // Updates the display to what the user has chosen to count down from
-    fun updateCount(ms:Long){
-        timeToCountDownInMs = ms
-        if(!isCounting) {
-            updateCountDownDisplay(ms)
-        }
-    }
-
-    // Starts the countdown
     fun startCountDown(v: View){
-        timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
+        isCounting = true
+
+        timer = object : CountDownTimer(workCountDownTimeInMs, timeTicks) {
             override fun onFinish() {
-                Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
-                startButton.isEnabled = true
+                Toast.makeText(this@MainActivity, "Slutt på jobbing", Toast.LENGTH_SHORT).show()
                 isCounting = false
+                if(workCountDownTimeInMs > 0)
+                    startPauseTimer(v)
+
+                else {
+                    isCounting = false
+                    timer.cancel()
+                    updateCountDownDisplay(0)
+                }
             }
+
 
             override fun onTick(millisUntilFinished: Long) {
                 updateCountDownDisplay(millisUntilFinished)
@@ -98,8 +122,45 @@ class MainActivity : AppCompatActivity() {
         timer.start()
     }
 
-    fun updateCountDownDisplay(timeInMs:Long){
-        coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+    fun startPauseTimer(v: View){
+
+        pauseTimer = object : CountDownTimer(pauseCountDownTimeInMs, timeTicks) {
+            override fun onFinish() {
+                Toast.makeText(this@MainActivity, "Pausen er ferdig", Toast.LENGTH_SHORT).show()
+                isCounting = false
+                if(numberOfLoops > 0){
+                    numberOfLoops--
+                    startCountDown(v)
+                }
+
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                updateCountDownDisplayPause(millisUntilFinished)
+            }
+        }
+        pauseTimer.start()
     }
+
+    fun updateCountDownDisplay(timeInMs: Long){
+        countdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+    }
+
+    fun updateCountDownDisplayPause(timeInMs: Long){
+        countdownDisplayPause.text = millisecondsToDescriptiveTime(timeInMs)
+    }
+
+    fun setWorkTimer(workInMilliseconds: Long){
+        workCountDownTimeInMs = workInMilliseconds
+        println(workCountDownTimeInMs)
+        // Updates the worktime when it's counting down
+        updateCountDownDisplay(workInMilliseconds)
+    }
+    fun setPauseTimer(pauseInMilliseconds: Long){
+        pauseCountDownTimeInMs = pauseInMilliseconds
+        // Updates the pausetime when it's counting down
+        updateCountDownDisplayPause(pauseInMilliseconds)
+    }
+
 
 }
